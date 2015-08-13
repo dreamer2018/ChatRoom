4ba0190d82dd2097008562dd8ae556f93182ffa2
dreamer2018 <zhoupans_mail@163.com>
2015-08-13 10:53:26 +0800

    colour_test.c

---
+++ colour_test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: colour_test.c
+	> Author: ZhouPan / git:dreamer2018
+	> Mail: zhoupans_mail@163.com
+	> Blog: blog.csdn.net/it_dream_er 
+	> Created Time: Tue 11 Aug 2015 07:15:11 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    printf("\033[31m hello world \033[0m\n");
+}
