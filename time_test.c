b243bb4d71a43ec0e1b1162317854feb42a76a84
dreamer2018 <zhoupans_mail@163.com>
2015-08-14 10:22:37 +0800

    debug

--- time_test.c
+++ time_test.c
@@ -27,45 +27,5 @@ void print_time(time_t st_time) //日期解析函数
 {
     struct tm *p;
     p=localtime(&st_time);
-    printf("%d-",p->tm_year+1900);
-    if(p->tm_mon<10)
-    {
-        printf("0%d-",p->tm_mon+1);
-    }
-    else
-    {
-        printf("%2d-",p->tm_mon+1);
-    }
-    if(p->tm_mday<10) //小于十的前面补零
-    {
-        printf("0%d",p->tm_mday); 
-    }
-    else
-    {
-        printf("%2d",p->tm_mday);        
-    }
-    if(p->tm_hour<10)
-    {
-        printf(" 0%d:",p->tm_hour);        
-    }
-    else
-    {
-       printf(" %2d:",p->tm_hour);        
-    }
-    if(p->tm_min<10)
-    {
-        printf("0%d",p->tm_min);        
-    }
-    else
-    {
-        printf("%2d",p->tm_min);           
-    }
-    if(p->tm_sec<10)
-    {
-        printf(":0%d\n",p->tm_sec);
-    }
-    else
-    {
-        printf(":%2d\n",p->tm_sec);
-    }
+    printf("%d-%.2d-%.2d %.2d:%.2d:%.2d",p->tm_year+1900,p->tm_mon+1,p->tm_mday,p->tm_hour,p->tm_min,p->tm_sec);
 }
