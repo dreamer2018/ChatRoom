2fc9270535d1380c61c9eacce9eb42292f9f27c1
dreamer2018 <zhoupans_mail@163.com>
2015-08-19 21:19:17 +0800

    add note

---
+++ backups/conio.h
@@ -0,0 +1,23 @@
+char getch()
+{
+    char c;
+    system("stty -echo");
+    system("stty -icanon");
+    
+    c=getchar();
+    
+    system("stty icanon");
+    system("stty echo");
+    return c;
+}
+
+char getche()
+{
+    char c;
+    system("stty -icanon");
+    c=getchar();
+    system("stty icanon");
+    return c; 
+}
+
+
